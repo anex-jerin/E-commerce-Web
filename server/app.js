1623f42515f334480b5839152b523f6132824b2b
Vikki <anexcruise@gmail.com>
2023-01-25 21:54:59 +0530

    second

---
+++ server/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+
+const PORT = process.env.PORT;
+
+async function connect() {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(process.env.MONGO_URI);
+  app.listen(PORT, () => {
+    console.log(`connected to port : ${PORT}`);
+  });
+}
+
+connect();
---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
